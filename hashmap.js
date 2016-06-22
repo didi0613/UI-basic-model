41b911028e13b8d8bf83c3fbe7d3b838e993c661
gina0613 <sdi@usc.edu>
2016-06-22 15:10:45 -0700

    Hashmap

--- hashmap.js
+++ hashmap.js
@@ -11,4 +11,13 @@ var hashMap = {
     Remove: function (key) {
         delete this[key]
     }
-}  
+};
+
+// Solution 2
+var map = {};
+// add a item
+map[key1] = value1;
+// or remove it
+delete map[key1];
+// or determine whether a key exists
+key1 in map;
