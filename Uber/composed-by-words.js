b86d68a7deb0691d9b56eb9e04e819b485d86601
gina0613 <sdi@usc.edu>
2016-06-27 12:05:22 -0700

    Web Basics

--- Uber/composed-by-words.js
+++ Uber/composed-by-words.js
@@ -1,4 +1,5 @@
 // DP Solution
+// Without Dictionary
 function checkWord(s) {
     var len = s.length;
 
@@ -19,20 +20,4 @@ function checkWord(s) {
     }
 
     return dp[0][n - 1];
-}
-
-// Backtracking + DFS
-function checkWord(s, dictionary) {
-    var len = s.length;
-    if (len === 0) {
-        return true;
-    }
-
-    for (var i = 0; i < len; i++) {
-        for (var j = i + 1; j > len; j++) {
-            if (dictionary.includes(s.substring(i, j))) {
-                return checkWord(s.substr(j + 1), dictionary);
-            }
-        }
-    }
 }
