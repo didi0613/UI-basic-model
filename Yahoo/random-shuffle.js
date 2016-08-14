4f5330ff5aabf5e4b8bd6c4e76c05baae0929fd0
gina0613 <sdi@usc.edu>
2016-08-14 14:10:29 -0700

    web basics

---
+++ Yahoo/random-shuffle.js
@@ -0,0 +1,17 @@
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+};
+
+console.log("xxxxx", shuffle([1,2,3,4,5]));
