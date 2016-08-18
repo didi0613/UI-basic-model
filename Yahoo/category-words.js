7f60f713f556ca5d9bf568438149c62aca179ca6
gina0613 <sdi@usc.edu>
2016-08-17 22:21:27 -0700

    Web Basics

---
+++ Yahoo/category-words.js
@@ -0,0 +1,27 @@
+var arr = ["stop", "post", "eat", "tea", "tops", "apple"];
+
+var sort = function (arr) {
+    var hashmap = {};
+    for (var i = 0; i < arr.length; i++) {
+        var temp = arr[i].split('').sort(function (a, b) {
+            return a < b;
+        }).join('');
+        debugger
+        if (hashmap[temp]) {
+            hashmap[temp].push(arr[i]);
+        } else {
+            hashmap[temp] = [arr[i]];
+        }
+    }
+
+    var str = "";
+    for (var item in hashmap) {
+        for (var object in hashmap[item]) {
+            str += hashmap[item][object] + " ";
+        }
+        str += "; "
+    }
+    return str;
+};
+
+console.log(sort(arr));
