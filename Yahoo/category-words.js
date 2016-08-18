eb7e1f6d7ca278450dbfbbcc41a9ef7a66032cc6
gina0613 <sdi@usc.edu>
2016-08-17 22:38:09 -0700

    Web Basics

--- Yahoo/category-words.js
+++ Yahoo/category-words.js
@@ -1,3 +1,4 @@
+// Use sorted string as index
 var arr = ["stop", "post", "eat", "tea", "tops", "apple"];
 
 var sort = function (arr) {
@@ -6,7 +7,7 @@ var sort = function (arr) {
         var temp = arr[i].split('').sort(function (a, b) {
             return a < b;
         }).join('');
-        
+
         if (hashmap[temp]) {
             hashmap[temp].push(arr[i]);
         } else {
@@ -25,3 +26,38 @@ var sort = function (arr) {
 };
 
 console.log(sort(arr));
+
+// use
+var arr = ["stop", "post", "eat", "tea", "tops", "apple"];
+
+function categorizeWords(arr) {
+    var hashmap = {};
+
+    for (var i = 0; i < arr.length; i++) {
+        var index = getKey(arr[i]);
+        if (hashmap[index]) {
+            hashmap[index].push(arr[i]);
+        } else {
+            hashmap[index] = [arr[i]];
+        }
+    }
+
+    var str = "";
+    for (var word in hashmap) {
+        for (var object in hashmap[word]) {
+            str += hashmap[word][object] + " ";
+        }
+        str += "; ";
+    }
+    return str;
+}
+
+function getKey(str) {
+    var sum = 0;
+    for (var i = 0; i < str.length; i++) {
+        sum += parseInt(str[i].charCodeAt());
+    }
+    return sum;
+}
+
+console.log(categorizeWords(arr));
