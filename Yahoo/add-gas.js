b308444f0d76b986b17bfde6a5f079ac6b30941c
gina0613 <sdi@usc.edu>
2016-08-28 15:14:21 -0700

    Web Basics

--- Yahoo/add-gas.js
+++ Yahoo/add-gas.js
@@ -3,9 +3,6 @@
  输入：gas station 位置 [X0, X1, X2 ... Xn]
  条件：car 加一次油最多能跑 100，要求停最少次数到终点 (Xn)
  输出：停车加油的位置 ArrayList<Integer>()
- 2.2、follow up
- 每个station 有 cost [C0, C1, C2...]
- 求使得 cost 最小，返回停车加油次数
  */
 function minAddGas(arr) {
     var n = arr.length;
@@ -26,3 +23,9 @@ function minAddGas(arr) {
 
 var arr = [100, 150, 200, 230, 300, 350, 400];
 console.log("xxxx", minAddGas(arr));
+
+/*
+ 2.2、follow up
+ 每个station 有 cost [C0, C1, C2...]
+ 求使得 cost 最小，返回停车加油次数
+ * */
