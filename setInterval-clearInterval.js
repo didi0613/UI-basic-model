1df81850a08042d350acb4350e974de092bc6a83
gina0613 <sdi@usc.edu>
2016-09-09 13:52:20 -0700

    web basics

---
+++ setInterval-clearInterval.js
@@ -0,0 +1,32 @@
+var timeoutId = null;
+
+function interval(func, wait, times) {
+    var interv = function (w, t) {
+        return function () {
+            if (typeof t === "undefined" || t-- > 0) {
+                timeoutId = setTimeout(interv, w);
+                try {
+                    func.call(null);
+                }
+                catch (e) {
+                    t = 0;
+                    throw e.toString();
+                }
+            }
+        }
+    }(wait, times);
+
+    setTimeout(interv, wait);
+}
+
+interval(function () {
+    $("#container").append("<div>Hello World</div>")
+}, 1000, 10);
+
+$("button").click(function () {
+    window.clearTimeout(timeoutId);
+});
+
+///////////////////HTML/////////////////////
+// <button>Clear Interval</button>
+// <div id="container"></div>
---
+++ throttle-debounce.js
@@ -0,0 +1,32 @@
+var throttle = function (callback, limit) {
+    var wait = false;
+    return function () {
+        if (!wait) {
+            wait = true;
+            callback.call();
+            setTimeout(function () {
+                wait = false;
+            }, limit);
+        }
+    }
+};
+
+var debounce = function (func, wait) {
+    var timeout;
+    return function () {
+        var later = function () {
+            timeout = null;
+            func.apply();
+        };
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+    };
+};
+
+$('button').click(debounce(function () {
+    $('#container').append('<div>hello world</div>');
+}, 3000));
+
+////////////////////////////HTML//////////////////////////////
+// <button>Throttle Button</button>
+// <div id="container"></div>
